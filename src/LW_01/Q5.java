bdd835fb3b1c90277b90c8c16a5bdf93f76d597d
LakminaRubasinghe <lakminas2003@gmail.com>
2025-09-14 21:31:45 +0530

    Finished answering Q5 (Using Circle Class).

--- src/LW_01/Q5.java
+++ src/LW_01/Q5.java
@@ -1,4 +1,23 @@
 package LW_01;
 
+import javax.swing.*;
+
 public class Q5 {
+    public static void main(String[] args) {
+        Circle outerCircle = new Circle(21);
+        Circle innerCircle = new Circle(7);
+        double outerCircleArea = outerCircle.computeArea();
+        double innerCircleArea = innerCircle.computeArea();
+        double shadedArea = outerCircleArea-innerCircleArea;
+
+        System.out.println("Shaded area : "+shadedArea);
+
+        double outerCircumference = outerCircle.computeCircumference();
+        double inerCircumference = innerCircle.computeCircumference();
+        double shadedCircumference = outerCircumference-inerCircumference;
+
+        System.out.println("Shaded circumference : "+shadedCircumference);
+
+
+    }
 }
