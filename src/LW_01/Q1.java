f76e1017aad199e47a13a70feff180ef90dcb542
LakminaRubasinghe <lakminas2003@gmail.com>
2025-09-14 16:25:02 +0530

    Finished answering Q1.

--- src/LW_01/Q1.java
+++ src/LW_01/Q1.java
@@ -1,4 +1,13 @@
 package LW_01;
+import java.util.*;
 
 public class Q1 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter a word that has odd number of characters : ");
+        String word=input.next();
+        int total=word.length();
+        total=(total/2);
+        System.out.println("The middle letter of the word is : "+word.charAt(total));
+    }
 }
