8155bb58af5bbb391aed1dca89e999407abb6469
LakminaRubasinghe <lakminas2003@gmail.com>
2025-09-14 16:29:34 +0530

    Finished answering Q2.

--- src/LW_01/Q2.java
+++ src/LW_01/Q2.java
@@ -1,4 +1,16 @@
 package LW_01;
 
+import java.util.*;
+
 public class Q2 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter first name : ");
+        String fname=input.next();
+        System.out.print("Enter middle name : ");
+        String mname=input.next();
+        System.out.print("Enter last name : ");
+        String lname=input.next();
+        System.out.println(lname+", "+fname+" "+mname.charAt(0)+".");
+    }
 }
