ba29da5149e974c3d35f75c25690c3b249ac1514
LakminaRubasinghe <lakminas2003@gmail.com>
2025-09-14 16:35:27 +0530

    Finished answering Q3.

--- src/LW_01/Q3.java
+++ src/LW_01/Q3.java
@@ -1,4 +1,15 @@
 package LW_01;
 
+import java.util.*;
 public class Q3 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter length in centimeters (without decimals) : ");
+        int centi=input.nextInt();
+        int feet=centi/30;
+        centi=centi%30;
+        double inches=centi/2.54;
+        System.out.printf("Entered length is %d feets and %.2f inches",feet, inches);
+    }
 }
+
