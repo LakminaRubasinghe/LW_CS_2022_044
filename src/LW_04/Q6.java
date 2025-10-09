e64723935eb9f60f21b784d28019230db2437a0f
LakminaRubasinghe <lakminas2003@gmail.com>
2025-10-09 21:20:03 +0530

    Finished answering Q6 of LW_04

--- src/LW_04/Q6.java
+++ src/LW_04/Q6.java
@@ -1,4 +1,12 @@
 package LW_04;
-
 public class Q6 {
+    public static void main(String[] args) {
+        int number=10;
+        while (number<50){
+            System.out.print(number+" ");
+            number++;
+            if(number%10==0)
+                System.out.println();
+        }
+    }
 }
