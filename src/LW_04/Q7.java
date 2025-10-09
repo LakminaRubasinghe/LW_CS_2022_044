d68f6e762cd07fa6eb537698b8119c6b6c8b10d9
LakminaRubasinghe <lakminas2003@gmail.com>
2025-10-09 21:21:48 +0530

    Finished answering Q7 of LW_04

--- src/LW_04/Q7.java
+++ src/LW_04/Q7.java
@@ -1,4 +1,23 @@
 package LW_04;
-
+import java.util.*;
 public class Q7 {
+    public static void main(String[] args) {
+        int number=0;
+        do {
+            Scanner scanner=new Scanner(System.in);
+            System.out.print("Enter a number to check the number of digits in it : ");
+            number=scanner.nextInt();
+            if(number>=0){
+                int count=1;
+                while(number/10!=0){
+                    number=number/10;
+                    count++;
+                }
+                System.out.println("There are "+count+" digits in the entered number.");
+            }else{
+                System.out.println("Entered number is invalid");break;
+            }
+
+        }while(number>=0);
+    }
 }
