f41e53e987d4e6b92cae62acb171b08a43c1389e
LakminaRubasinghe <lakminas2003@gmail.com>
2025-10-09 20:17:25 +0530

    Finished answering Q1 of LW_02

--- src/LW_02/Q1.java
+++ src/LW_02/Q1.java
@@ -1,9 +1,7 @@
 package LW_02;
-import java.util.*;
 
 public class Q1 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
         for (int number=10; number<50; number++){
             if (number%10==0){
                 System.out.println();
