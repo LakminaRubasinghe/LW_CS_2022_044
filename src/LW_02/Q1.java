cd8b31b8418896607d198e8df4d475f18e20709e
LakminaRubasinghe <lakminas2003@gmail.com>
2025-10-09 20:16:08 +0530

    Finished answering Q1 of LW_02

--- src/LW_02/Q1.java
+++ src/LW_02/Q1.java
@@ -1,4 +1,14 @@
 package LW_02;
+import java.util.*;
 
 public class Q1 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        for (int number=10; number<50; number++){
+            if (number%10==0){
+                System.out.println();
+            }
+            System.out.print(number+" ");
+        }
+    }
 }
