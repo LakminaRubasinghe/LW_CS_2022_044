df67fda6710166b7b2c5aa206482c5ec4110d359
LakminaRubasinghe <lakminas2003@gmail.com>
2025-10-11 00:07:12 +0530

    Finished answering Q6 of LW_02

--- src/LW_02/Q6.java
+++ src/LW_02/Q6.java
@@ -1,4 +1,9 @@
 package LW_02;
-
+import java.time.Year;
+import java.util.*;
 public class Q6 {
+    public static void main(String[] args) {
+        GregorianCalendar myDate=new GregorianCalendar(2025,8,14);
+        System.out.println(myDate.getTime());
+    }
 }
