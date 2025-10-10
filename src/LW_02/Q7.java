25bd456272d70928c7c21cafd8c86aa2fb0360a4
LakminaRubasinghe <lakminas2003@gmail.com>
2025-10-11 00:08:46 +0530

    Finished answering Q7 of LW_02

--- src/LW_02/Q7.java
+++ src/LW_02/Q7.java
@@ -1,4 +1,10 @@
 package LW_02;
-
+import java.text.SimpleDateFormat;
+import java.util.*;
 public class Q7 {
+    public static void main(String[] args) {
+        Date date=new Date(2025-1900,8,14);
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("EEEE, MMMM d, YYYY");
+        System.out.println(simpleDateFormat.format(date));
+    }
 }
